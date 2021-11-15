42add088cef10d3cf4bbcc9eebff04fa663e91b1
David Moll <david.leander.moll@gmail.com>
2021-11-15 15:50:13 +0100

    Added code/comments for 2.15

--- app.ts
+++ app.ts
@@ -1,9 +1,3 @@
-/*
-The parameters of this function need to two numbers and one boolean
-Changing any of the parameneters into a string would
-result into an error during compilation
-*/
-
 function add(n1: number, n2: number, showResult: boolean) {
 	if (showResult) {
 		console.log(n1 + n2);
@@ -11,16 +5,24 @@ function add(n1: number, n2: number, showResult: boolean) {
 	return n1 + n2;
 }
 
+/*
+TS perfectly knows what type a variable is by its value.
+Writing for example "const number1: number = 5;" is redundant
+since TS already knows that number1 is a number by the assigned value
+*/
+
 const number1 = 5;
 const number2 = 2.8;
 
+/*
+When declaring a variable but not assigning any value it is considered
+best practise to still write the type of it
+*/ 
+
+let number3: number; 
+
 console.log(`Number 1 is a ${typeof number1} with a value of ${number1}`);
 console.log(`Number 2 is a ${typeof number2} with a value of ${number2}`);
-
-/*
-If printResult is true the result will be shown in the console
-printResult = false will still calculate the result but not show it
-*/
 
 const printResult = false;
 
