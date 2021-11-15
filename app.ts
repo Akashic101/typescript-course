597f95e4273ef4531a4053e96c68e5610bb5587d
David Moll <david.leander.moll@gmail.com>
2021-11-15 15:29:55 +0100

    Added code/comments for 2.14

--- app.ts
+++ app.ts
@@ -1,13 +1,14 @@
 /*
-
-This is for section 2.12: https://github.com/Akashic101/typescript-course/commit/dbf020f897745723bd958ae6c3720ebd625d5224
-
-The parameters of this function need to be number
+The parameters of this function need to two numbers and one boolean
 Changing any of the parameneters into a string would
 result into an error during compilation
 */
-function add(n1: number, n2: number) {
-  return n1 + n2;
+
+function add(n1: number, n2: number, showResult: boolean) {
+	if (showResult) {
+		console.log(n1 + n2);
+	}
+	return n1 + n2;
 }
 
 const number1 = 5;
@@ -16,5 +17,11 @@ const number2 = 2.8;
 console.log(`Number 1 is a ${typeof number1} with a value of ${number1}`);
 console.log(`Number 2 is a ${typeof number2} with a value of ${number2}`);
 
-const result = add(number1, number2);
-console.log(`${number1} + ${number2} = ${result}`);
+/*
+If printResult is true the result will be shown in the console
+printResult = false will still calculate the result but not show it
+*/
+
+const printResult = false;
+
+add(number1, number2, printResult);
