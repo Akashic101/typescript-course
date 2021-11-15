b53eff42b26ee7333f866681af076d49acd509c0
David Moll <david.leander.moll@gmail.com>
2021-11-15 16:56:06 +0100

    Added code/comments for 2.20

--- app.ts
+++ app.ts
@@ -1,41 +1,27 @@
-const person1 = {
+/*
+enums are space-efficient and you can also change
+the numbers of the elements
+by giving them a different identifier (default is 0)
+
+enum Role { ADMIN = 5, READ_ONLY, AUTHOR = 'AUTHOR'};
+
+ADMIN will be 5
+READ_ONLY will be 6
+AUTHOR will be AUTHOR
+*/
+
+enum Role { ADMIN, READ_ONLY, AUTHOR};
+
+const person = {
 	name: "David",
 	age: 23,
 	hobbies: ["Motorsports", "Gaming"],
-	role: [2, "Frontend-Developer"],
+	role: Role.ADMIN,
 };
 
-/*
-TS does not know that the role-array is of a fixed length. Currently
-it can be of type number or string which is why the two functions will work
-*/
-
-person1.role.push("Gamer");
-person1[1] = 10;
-
-
-
-const person2: {
-	name: string;
-	age: number;
-	hobbies: string[];
-	role: [number, string]; //This is a tuple; fixed length, fixed type
-} = {
-	name: 'David',
-	age: 23,
-	hobbies: ['Motorsports', 'Gaming'],
-	role: [2, 'Frontend-Developer'],
-};
-
-
-//array.push is an exception which TS will not catch
-
-person2.role.push('Gamer'); 
-
-/*
-This will not work since the second element must
-explicitly be a string
-*/
-
-person2.role[1] = 10; 
-
+if(person.role === Role.ADMIN) {
+	console.log(`User ${person.name} is ADMIN`)
+}
+else {
+	console.log(`User ${person.name} is not an ADMIN`)
+}
