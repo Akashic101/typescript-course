9b79a92b2e1278cd7b98b7ee9b2763bae03d813f
David Moll <david.leander.moll@gmail.com>
2021-11-16 10:24:48 +0100

    Added code/comments for 2.27

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-/*
-Adding a ": number" behind the parameters will define the type of
-the return-value. Not returning anything will be defined with ": void"
-*/
 function add(n1, n2) {
     return n1 + n2;
 }
-/*
-This function is a void-example. There is no return-value so
-the functions return-value is "void". Use "void" and not "undefined".
-"Undefined" means there is an empty return-value. "Void" is used when
-there is no return-value at all.
-*/
 function printResult(num) {
     console.log("Result" + num);
 }
 printResult(add(5, 23));
+/*
+This variable is designed for a function which has two parameters,
+both numbers, and the return-value must also be a number.
+This is why there is no error for the function add() but for the
+printResult()-function since the description does not fit
+*/
+var combineValue;
+combineValue = add;
+// combineValue = printResult;
+console.log(combineValue(8, 8));
