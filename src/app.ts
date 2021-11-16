d81120b914ee6a6965ab5f11bfeb6e4c6fb17c42
David Moll <david.leander.moll@gmail.com>
2021-11-16 14:09:41 +0100

    Added code/comments for 4.54

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,38 @@
 /*
-Rest Parameters are parameters where you don't know how many
-you will need and/or get.
+Array destructering works by pulling out each element of an array from left-to-right
+and storing it in a variable assigned on the left side of the equal-sign
 
-The reduce-function takes all values of an array and reduces it
-a single one. "currentResult" is by default 0 and can also be defined
-by the number afte the parameters, currentValue is the
-current element of the array the function is at at the moment. 
+You can also use the spread-operator to store the rest of the elements which have
+not been assigned in a new array. The original array will not be changed
 */
-const add = (...numbers: number[]) => {
-    return numbers.reduce((currentResult, currentValue) => {
-        return currentResult + currentValue;
-    }, 0);
+
+const hobbies: string[] = ["gaming", "motorsports", "biking", "reading"];
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log({hobby1});
+console.log({remainingHobbies});
+
+/*
+This way of destructering also works on objects
+*/
+
+const person = {
+	firstName: "David",
+	age: 23,
 };
 
-const addedNumbers = add(5, 1, 6, 7, 3, 5);
+/*
+This is not Type infering but an alias. The variable is not of type userName
+but saved as userName
+*/
 
-console.log(addedNumbers);
+const { firstName: userName, age } = person;
+
+console.log({userName});
+console.log({age});
+
+/*
+Writing a variable in curly-brackets like {this} gives out not only the value
+of the variable but also the name for easier identifying. Isn't that neat?
+*/
