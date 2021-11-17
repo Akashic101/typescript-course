6c0c6b7ec468a6c2d64ba6d5bb399bd6e6f4e7c9
David Moll <david.leander.moll@gmail.com>
2021-11-17 15:25:20 +0100

    Added code/comments for 6.84

--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,62 @@
 "use strict";
+const e1 = {
+    name: "David",
+    privilege: ["create_server"],
+    startDate: new Date(),
+};
 /*
-You can mark a parameter as optional by putting a ? behind the name
-This also works with functions: myMethod?() {...}
+The if-statement is called a "Type Guard" it will make sure that
+functions will work, even when the arguments are of different types
 */
-class Person {
-    constructor(n) {
-        this.age = 23;
-        if (n) {
-            this.name = n;
-        }
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
     }
-    greet(phrase) {
-        if (this.name) {
-            console.log(phrase + " " + this.name);
-        }
-        else {
-            console.log("hi");
-        }
+    else {
+        return +a + +b;
     }
 }
-let user1 = new Person("David");
-let user2 = new Person();
-user1.greet("Hello, I'm"); //User1 has a name, so it uses the greet-function
-user2.greet("What's up, my name is"); //User2 has no name so it will just say "hi"
-console.log({ user1 });
-console.log({ user2 });
+function printEmployeeInfo(emp) {
+    console.log("Name: " + emp.name); //this works since both types have a name-field
+    /*
+    This needs another type of type-check with the "in"-keywoard.
+    It checks if the field you are looking for is in the object you want to check
+    and allows you to use it inside the if-statement
+    */
+    if ("privilege" in emp) {
+        console.log("Privileges: " + emp.privilege);
+    }
+    if ("startDate" in emp) {
+        console.log("startDate: " + emp.startDate);
+    }
+}
+printEmployeeInfo(e1);
+class Car {
+    drive() {
+        console.log("Driving in a car...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving in a truck...");
+    }
+    loadCargo(amount) {
+        console.log("Loading " + amount + " of cargo");
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+/*
+This is another type of type-check called "instanceof". It tells you
+if an object is an instance of a certain type. "Instanceof" is
+vanilla JS and also available in TS
+*/
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,106 @@
+type Admin = {
+	name: string;
+	privilege: string[];
+};
+
+type Employee = {
+	name: string;
+	startDate: Date;
+};
+
+/*
+You can create new types by merging two others into one, which
+is called an intersection type.
+When creating an object like this it must have all fields
+of both types it got merged from
+*/
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+	name: "David",
+	privilege: ["create_server"],
+	startDate: new Date(),
+};
+
+/*
+In case of a union-type it will make the new type the one
+both have in common, or if multiple it will create a union-type
+*/
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+/*
+The if-statement is called a "Type Guard" it will make sure that
+functions will work, even when the arguments are of different types
+*/
+
+function add(a: Combinable, b: Combinable) {
+	if (typeof a === "string" || typeof b === "string") {
+		return a.toString() + b.toString();
+	} else {
+		return +a + +b;
+	}
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+	console.log("Name: " + emp.name); //this works since both types have a name-field
+
+	/*
+	This needs another type of type-check with the "in"-keywoard.
+	It checks if the field you are looking for is in the object you want to check
+	and allows you to use it inside the if-statement
+	*/
+
+	if ("privilege" in emp) {
+		console.log("Privileges: " + emp.privilege);
+	}
+	if ("startDate" in emp) {
+		console.log("startDate: " + emp.startDate);
+	}
+}
+
+printEmployeeInfo(e1);
+
+
+class Car {
+	drive() {
+		console.log("Driving in a car...");
+	}
+}
+
+class Truck {
+	drive() {
+		console.log("Driving in a truck...");
+	}
+
+	loadCargo(amount: number) {
+		console.log("Loading " + amount + " of cargo");
+	}
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+/*
+This is another type of type-check called "instanceof". It tells you
+if an object is an instance of a certain type. "Instanceof" is 
+vanilla JS and also available in TS
+*/
+
+function useVehicle(vehicle: Vehicle) {
+	vehicle.drive();
+	if(vehicle instanceof Truck) {
+		vehicle.loadCargo(1000);
+	}
+}
+
+useVehicle(v1);
+useVehicle(v2);
