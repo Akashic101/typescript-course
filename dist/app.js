68026504d533f19eb3f4392ce50405b6902c4e3a
David Moll <david.leander.moll@gmail.com>
2021-11-17 14:34:41 +0100

    Added code/comments for 5.78

--- dist/app.js
+++ dist/app.js
@@ -1,41 +1,28 @@
 "use strict";
 /*
-Interfaces can define the type of a function as well. Here we have
-a interface which has two arguments, both of type number, and a return-value
-of type number as well
-
-We can then create a new functions of the custom type of the interface
-*/
-let add = (n1, n2) => {
-    return n1 + n2;
-};
-/*
-Implementing an interface forces the class to adhere to the interface. The class
-needs to have the same fields and functions as the interface,
-but you can also add your own fields and functions inside the class
-on top of that.
-You can implement more than one interface (seperated by a comma), not like with classes
+You can mark a parameter as optional by putting a ? behind the name
+This also works with functions: myMethod?() {...}
 */
 class Person {
     constructor(n) {
         this.age = 23;
-        this.name = n;
+        if (n) {
+            this.name = n;
+        }
     }
     greet(phrase) {
-        console.log(phrase + " " + this.name);
+        if (this.name) {
+            console.log(phrase + " " + this.name);
+        }
+        else {
+            console.log("hi");
+        }
     }
 }
-/*
-This object is of the class Person, but is of type Greetable at the same time
-since the class Person implements the interface Greetable
-*/
 let user1 = new Person("David");
-/*
-You don't need to make the name-field in the class Person readonly as well since
-this is already implemented by the interface
-
-user1.name = 'Ben'; //This is not possible since the field is readonly
-*/
-user1.greet("Hello, I'm");
+let user2 = new Person();
+user1.greet("Hello, I'm"); //User1 has a name, so it uses the greet-function
+user2.greet("What's up, my name is"); //User2 has no name so it will just say "hi"
 console.log({ user1 });
+console.log({ user2 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,41 @@
 /*
-Interfaces can define the type of a function as well. Here we have
-a interface which has two arguments, both of type number, and a return-value
-of type number as well
-
-We can then create a new functions of the custom type of the interface
+You can mark a parameter as optional by putting a ? behind the name
+This also works with functions: myMethod?() {...}
 */
-
-interface AddFn {
-	(a: number, b: number): number;
-}
-
-let add: AddFn = (n1: number, n2: number) => {
-	return n1 + n2;
-};
-
-
 
 interface Named {
-	readonly name: String;
+	readonly name?: string; //Both are optional
+	outputName?: string;
 }
-
-/*
-You can extend interfaces meaning that one interface is extended from another
-and whatever class implements the interface needs to satisfy the requirements of both
-interfaces
-*/
 
 interface Greetable extends Named {
 	greet(phrase: string): void;
 }
 
-/*
-Implementing an interface forces the class to adhere to the interface. The class
-needs to have the same fields and functions as the interface,
-but you can also add your own fields and functions inside the class
-on top of that.
-You can implement more than one interface (seperated by a comma), not like with classes
-*/
-
-class Person implements Greetable, Named {
-	name: string;
+class Person implements Greetable {
+	name?: string; //This is now optional as well
 	age = 23;
 
-	constructor(n: string) {
-		this.name = n;
+	constructor(n?: string) {
+		if (n) {
+			this.name = n;
+		}
 	}
 
 	greet(phrase: string) {
-		console.log(phrase + " " + this.name);
+		if (this.name) {
+			console.log(phrase + " " + this.name);
+		} else {
+			console.log("hi");
+		}
 	}
 }
 
-/*
-This object is of the class Person, but is of type Greetable at the same time
-since the class Person implements the interface Greetable
-*/
-
 let user1: Greetable = new Person("David");
+let user2: Greetable = new Person();
 
-/*
-You don't need to make the name-field in the class Person readonly as well since
-this is already implemented by the interface
+user1.greet("Hello, I'm"); //User1 has a name, so it uses the greet-function
+user2.greet("What's up, my name is"); //User2 has no name so it will just say "hi"
 
-user1.name = 'Ben'; //This is not possible since the field is readonly
-*/
-
-user1.greet("Hello, I'm");
 console.log({ user1 });
+console.log({ user2 });
