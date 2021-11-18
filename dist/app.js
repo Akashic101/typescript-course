bd6561ca65fbd6c35b0e9896f38744ab16a40f31
David Moll <david.leander.moll@gmail.com>
2021-11-18 10:52:38 +0100

    Added code/comments for 8.105

--- dist/app.js
+++ dist/app.js
@@ -1,21 +1,30 @@
 "use strict";
-function createCourseGoal(title, description, date) {
-    /*
-    Making this object Partial means that every variable is temporarily optional at this point.
-    We can only return it though as a courseGoal at the end when everything is complete
-    so we have to typecast it. This can be done so we can for example check step-by-step
-    if all the informations are correct, the title and description being over a certain
-    length or the goal completeUnit-date being in the future
-    */
-    let courseGoal = {};
-    courseGoal.title = title;
-    courseGoal.description = description;
-    courseGoal.completeUntit = date;
-    return courseGoal;
+/*
+This is a decorator-function (only available with ES6 as a target)
+It needs an argument
+*/
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logger(constructor) {
+    console.log('Logging...');
+    console.log(constructor);
 }
 /*
-Readonly is also a Utility type
-This array is readonly meaning we cannot push or pop anything to it
+You call a decorator by putting it infront of a class for example
+with an @-symbol
 */
-const names = ["David", "Ben"];
+let Person = class Person {
+    constructor() {
+        this.name = 'David';
+        console.log('Creating new user...');
+    }
+};
+Person = __decorate([
+    Logger
+], Person);
+const person = new Person();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,25 @@
-interface CourseGoal {
-	title: string;
-	description: string;
-	completeUntit: Date;
-}
+/*
+This is a decorator-function (only available with ES6 as a target)
+It needs an argument 
+*/
 
-function createCourseGoal(
-	title: string,
-	description: string,
-	date: Date
-): CourseGoal {
-	/*
-	Making this object Partial means that every variable is temporarily optional at this point.
-	We can only return it though as a courseGoal at the end when everything is complete
-	so we have to typecast it. This can be done so we can for example check step-by-step
-	if all the informations are correct, the title and description being over a certain
-	length or the goal completeUnit-date being in the future
-	*/
-
-	let courseGoal: Partial<CourseGoal> = {};
-	courseGoal.title = title;
-	courseGoal.description = description;
-	courseGoal.completeUntit = date;
-	return courseGoal as CourseGoal;
+function Logger(constructor: Function) {
+	console.log('Logging...');
+	console.log(constructor);
 }
 
 /*
-Readonly is also a Utility type
-This array is readonly meaning we cannot push or pop anything to it
+You call a decorator by putting it infront of a class for example
+with an @-symbol
 */
 
-const names: Readonly<string[]> = ["David", "Ben"];
+@Logger
+class Person {
+	name = 'David';
+
+	constructor() {
+		console.log('Creating new user...');
+	}
+}
+
+const person = new Person();
