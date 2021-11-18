741556d122e6e1be0e804a2d6b5819c2341419be
David Moll <david.leander.moll@gmail.com>
2021-11-18 15:22:58 +0100

    Added code/comments for 9.122

--- dist/app.js
+++ dist/app.js
@@ -1,109 +1,15 @@
 "use strict";
-/*
-This Descriptor autobinds the this-value not to the target, but to
-the object
-
-Target and methodName are not needed and optional and can be marked
-with an _ so TS still compiles the code
-*/
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-function Autobind(_target, _methodName, descriptor) {
-    const originalMethod = descriptor.value;
-    const adjustedDescriptor = {
-        //configurable: true, //Those two are optional and just
-        //enumerable: false,  //Show what is possible
-        get() {
-            const boundFunction = originalMethod.bind(this);
-            return boundFunction;
-        },
-    };
-    return adjustedDescriptor;
-}
-class Printer {
+class ProjectInput {
     constructor() {
-        this.message = "This works";
+        this.templateElement = document.getElementById('project-input'); //The template-element inside the HTML-file
+        this.hostElement = document.getElementById('app'); //The host-div which will display all informations from the template
+        const importedNode = document.importNode(this.templateElement.content, true); //Imports the content of the template with all nested elements
+        this.element = importedNode.firstElementChild; //Saves the next tag (first child) of the template
+        this.attach();
     }
-    showMessage() {
-        console.log(this.message);
-    }
-}
-__decorate([
-    Autobind
-], Printer.prototype, "showMessage", null);
-const p = new Printer();
-const button = document.querySelector("button");
-/*
-This would return undefined without the Decorator. "this" in showMessage() is bound to the
-target, in this case the button, not the Printer-object p.
-*/
-button.addEventListener("click", p.showMessage);
-/*
-This will work since the bind()-method changed "this" from the button
-to the Printer-object
-*/
-button.addEventListener("click", p.showMessage.bind(p));
-const registeredValidators = {};
-function Required(target, propName) {
-    var _a, _b;
-    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: [...((_b = (_a = registeredValidators[target.constructor.name]) === null || _a === void 0 ? void 0 : _a[propName]) !== null && _b !== void 0 ? _b : []), 'required'] });
-}
-function PositiveNumber(target, propName) {
-    var _a, _b;
-    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: [...((_b = (_a = registeredValidators[target.constructor.name]) === null || _a === void 0 ? void 0 : _a[propName]) !== null && _b !== void 0 ? _b : []), 'positive'] });
-}
-function validate(obj) {
-    const objectValidatorConfig = registeredValidators[obj.constructor.name];
-    if (!objectValidatorConfig) {
-        return true;
-    }
-    else {
-        let isValid = true;
-        for (const property in objectValidatorConfig) {
-            for (const validator of objectValidatorConfig[property]) {
-                switch (validator) {
-                    case "required":
-                        isValid = isValid && !!obj[property]; // !! means real true-or-false value
-                        break;
-                    case "positive":
-                        isValid && obj[property] > 0;
-                        break;
-                }
-            }
-        }
-        return isValid;
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element); //Define where to attach the element (the form) inside the template
     }
 }
-class Course {
-    constructor(title, price) {
-        this.title = title;
-        this.price = price;
-    }
-}
-__decorate([
-    Required
-], Course.prototype, "title", void 0);
-__decorate([
-    PositiveNumber
-], Course.prototype, "price", void 0);
-const courseForm = document.querySelector("form");
-courseForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const titleElement = document.getElementById("title");
-    const priceElement = document.getElementById("price");
-    const title = titleElement.value;
-    const price = +priceElement.value;
-    const createdCourse = new Course(title, price);
-    if (!validate(createdCourse)) {
-        alert("Invalid input - Please show again");
-        return;
-    }
-    else {
-        console.log(createdCourse);
-    }
-});
+const projectInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,127 +1,20 @@
-/*
-This Descriptor autobinds the this-value not to the target, but to
-the object
+class ProjectInput {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLFormElement;
 
-Target and methodName are not needed and optional and can be marked
-with an _ so TS still compiles the code
-*/
+	constructor() {
+		this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement; //The template-element inside the HTML-file
+		this.hostElement = document.getElementById('app')! as HTMLDivElement; //The host-div which will display all informations from the template
+	
+		const importedNode = document.importNode(this.templateElement.content, true); //Imports the content of the template with all nested elements
+		this.element = importedNode.firstElementChild as HTMLFormElement; //Saves the next tag (first child) of the template
+		this.attach();
+	}
 
-function Autobind(
-	_target: any,
-	_methodName: string,
-	descriptor: PropertyDescriptor
-) {
-	const originalMethod = descriptor.value;
-	const adjustedDescriptor: PropertyDescriptor = {
-		//configurable: true, //Those two are optional and just
-		//enumerable: false,  //Show what is possible
-		get() {
-			const boundFunction = originalMethod.bind(this);
-			return boundFunction;
-		},
-	};
-	return adjustedDescriptor;
-}
-
-class Printer {
-	message = "This works";
-
-	@Autobind
-	showMessage() {
-		console.log(this.message);
+	private attach() {
+		this.hostElement.insertAdjacentElement('afterbegin', this.element); //Define where to attach the element (the form) inside the template
 	}
 }
 
-const p = new Printer();
-
-const button = document.querySelector("button")!;
-
-/*
-This would return undefined without the Decorator. "this" in showMessage() is bound to the
-target, in this case the button, not the Printer-object p.
-*/
-
-button.addEventListener("click", p.showMessage);
-
-/*
-This will work since the bind()-method changed "this" from the button
-to the Printer-object
-*/
-
-button.addEventListener("click", p.showMessage.bind(p));
-
-interface ValidatorConfig {
-	[property: string]: {
-		//index-type notation
-		[validatableProperty: string]: string[]; //['required', 'positive'] for example
-	};
-}
-
-const registeredValidators: ValidatorConfig = {};
-
-function Required(target: any, propName: string) {
-    registeredValidators[target.constructor.name] = {
-        ...registeredValidators[target.constructor.name],
-        [propName]: [...(registeredValidators[target.constructor.name]?.[propName] ?? []), 'required']
-    };
-}
-
-function PositiveNumber(target: any, propName: string) {
-    registeredValidators[target.constructor.name] = {
-        ...registeredValidators[target.constructor.name],
-        [propName]: [...(registeredValidators[target.constructor.name]?.[propName] ?? []), 'positive']
-    };
-}
-
-function validate(obj: any) {
-	const objectValidatorConfig = registeredValidators[obj.constructor.name];
-	if (!objectValidatorConfig) {
-		return true;
-	} else {
-		let isValid = true;
-		for (const property in objectValidatorConfig) {
-			for (const validator of objectValidatorConfig[property]) {
-				switch (validator) {
-					case "required":
-						isValid = isValid && !!obj[property]; // !! means real true-or-false value
-						break;
-					case "positive":
-						isValid && obj[property] > 0;
-						break;
-				}
-			}
-		}
-		return isValid;
-	}
-}
-
-class Course {
-	@Required
-	title: string;
-	@PositiveNumber
-	price: number;
-
-	constructor(title: string, price: number) {
-		this.title = title;
-		this.price = price;
-	}
-}
-
-const courseForm = document.querySelector("form")!;
-courseForm.addEventListener("submit", (event) => {
-	event.preventDefault();
-	const titleElement = document.getElementById("title")! as HTMLInputElement;
-	const priceElement = document.getElementById("price")! as HTMLInputElement;
-
-	const title = titleElement.value;
-	const price = +priceElement.value;
-
-	const createdCourse = new Course(title, price);
-
-	if (!validate(createdCourse)) {
-		alert("Invalid input - Please show again");
-		return;
-	} else {
-		console.log(createdCourse);
-	}
-});
+const projectInput = new ProjectInput();
