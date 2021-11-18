90412a451bf78989ce2b931c3c7acaa6ffe35681
David Moll <david.leander.moll@gmail.com>
2021-11-18 15:39:02 +0100

    Added code/comments for 9.123

--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,24 @@ class ProjectInput {
         this.hostElement = document.getElementById('app'); //The host-div which will display all informations from the template
         const importedNode = document.importNode(this.templateElement.content, true); //Imports the content of the template with all nested elements
         this.element = importedNode.firstElementChild; //Saves the next tag (first child) of the template
-        this.attach();
+        // Since this element gets created during runtime it does not have an id. Giving
+        // it a id will make sure it gets affected by the css-file  
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title'); //Selects the input-field with the id "title"
+        this.descriptionInputElement = this.element.querySelector('#description'); //Selects the input-field with the id "description"
+        this.peopleInputElement = this.element.querySelector('#people'); //Selects the input-field with the id "people"
+        this.configure();
+        this.attach(); //this function must be called last
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element); //Define where to attach the element (the form) inside the template
+    }
+    submitHandler(event) {
+        event.preventDefault(); //prevents the default action to happen (HTTP-request)
+        console.log(this.titleInputElement.value); //prints out the value of the titleInputElement
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this)); //Adds an event-listener (submit) to the class and binds it to itself, not the button
     }
 }
 const projectInput = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
 
 	constructor() {
 		this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement; //The template-element inside the HTML-file
@@ -9,11 +12,31 @@ class ProjectInput {
 	
 		const importedNode = document.importNode(this.templateElement.content, true); //Imports the content of the template with all nested elements
 		this.element = importedNode.firstElementChild as HTMLFormElement; //Saves the next tag (first child) of the template
-		this.attach();
+		
+		// Since this element gets created during runtime it does not have an id. Giving
+		// it a id will make sure it gets affected by the css-file  
+		
+		this.element.id = 'user-input'; 
+		
+		this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement; //Selects the input-field with the id "title"
+		this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement; //Selects the input-field with the id "description"
+		this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement; //Selects the input-field with the id "people"
+
+		this.configure();
+		this.attach(); //this function must be called last
 	}
 
 	private attach() {
 		this.hostElement.insertAdjacentElement('afterbegin', this.element); //Define where to attach the element (the form) inside the template
+	}
+
+	private submitHandler(event: Event) {
+		event.preventDefault(); //prevents the default action to happen (HTTP-request)
+		console.log(this.titleInputElement.value); //prints out the value of the titleInputElement
+	}
+
+	private configure() {
+		this.element.addEventListener('submit', this.submitHandler.bind(this)); //Adds an event-listener (submit) to the class and binds it to itself, not the button
 	}
 }
 
