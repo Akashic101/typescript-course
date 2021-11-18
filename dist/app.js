39619b209d90d48d9effd4192100003514425293
David Moll <david.leander.moll@gmail.com>
2021-11-18 11:34:28 +0100

    Added code/comments for 8.108

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,4 @@
 "use strict";
-/*
-This functions gets an element by its ID in the HTML-body and saves it in the hookEl-const.
-You can then create a new person with the constructor of the class Person.
-If hookEl exists we can access its inner HTML-properties and set it to the template which is
-"<h1> My Person Object</h1>"
-The text of hookEl can then be changed to the name of the person-object which was created earlier
-*/
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,3 @@
-/*
-This functions gets an element by its ID in the HTML-body and saves it in the hookEl-const.
-You can then create a new person with the constructor of the class Person.
-If hookEl exists we can access its inner HTML-properties and set it to the template which is
-"<h1> My Person Object</h1>"
-The text of hookEl can then be changed to the name of the person-object which was created earlier
-*/
-
 function WithTemplate(template: string, hookId: string) {
 	return function (constructor: any) {
 		const hookEl = document.getElementById(hookId);
