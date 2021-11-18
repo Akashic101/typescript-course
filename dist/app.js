0c83d9f603744b10391793d86a8eaa7aa517a4ad
David Moll <david.leander.moll@gmail.com>
2021-11-18 10:24:16 +0100

    Added code/comments for 7.101

--- dist/app.js
+++ dist/app.js
@@ -1,52 +1,21 @@
 "use strict";
-/*
-This makes sure that the function only works with primitive types
-Creating an object based on this class would fail since an object
-is reference-based
-*/
-class DataStorage {
-    constructor() {
-        this.data = [];
-    }
-    addItem(item) {
-        this.data.push(item);
-    }
-    remoteItem(item) {
-        /*
-        This works only with primitive values
-        When working with objects this would fail
-        since when creating an object it creates a new
-        one in the memory, but we access the old one
-        */
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-    getItems() {
-        return [...this.data];
-    }
+function createCourseGoal(title, description, date) {
+    /*
+    Making this object Partial means that every variable is temporarily optional at this point.
+    We can only return it though as a courseGoal at the end when everything is complete
+    so we have to typecast it. This can be done so we can for example check step-by-step
+    if all the informations are correct, the title and description being over a certain
+    length or the goal completeUnit-date being in the future
+    */
+    let courseGoal = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntit = date;
+    return courseGoal;
 }
 /*
-Here the generic function works with a string
+Readonly is also a Utility type
+This array is readonly meaning we cannot push or pop anything to it
 */
-const textStorage = new DataStorage();
-textStorage.addItem("Babies first textStorage");
-textStorage.addItem("A lying cake");
-textStorage.remoteItem("A lying cake");
-console.log(textStorage.getItems());
-/*
-Here the generic function works with a number
-*/
-const numberStorage = new DataStorage();
-numberStorage.addItem(33);
-numberStorage.addItem(44);
-numberStorage.remoteItem(44);
-console.log(numberStorage.getItems());
-/*
-Union types also work as an example
-*/
-const unionStorage = new DataStorage();
-/*
-This will not work since objects are not accepted as a datatype
-
-const objStorage = new DataStorage<object>();
-*/
+const names = ["David", "Ben"];
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,32 @@
-/*
-This makes sure that the function only works with primitive types
-Creating an object based on this class would fail since an object
-is reference-based
-*/
+interface CourseGoal {
+	title: string;
+	description: string;
+	completeUntit: Date;
+}
 
-class DataStorage<T extends string | number | boolean> {
-	private data: T[] = [];
+function createCourseGoal(
+	title: string,
+	description: string,
+	date: Date
+): CourseGoal {
+	/*
+	Making this object Partial means that every variable is temporarily optional at this point.
+	We can only return it though as a courseGoal at the end when everything is complete
+	so we have to typecast it. This can be done so we can for example check step-by-step
+	if all the informations are correct, the title and description being over a certain
+	length or the goal completeUnit-date being in the future
+	*/
 
-	addItem(item: T) {
-		this.data.push(item);
-	}
-
-	remoteItem(item: T) {
-		/*
-		This works only with primitive values
-		When working with objects this would fail
-		since when creating an object it creates a new
-		one in the memory, but we access the old one
-		*/
-		this.data.splice(this.data.indexOf(item), 1);
-	}
-
-	getItems() {
-		return [...this.data];
-	}
+	let courseGoal: Partial<CourseGoal> = {};
+	courseGoal.title = title;
+	courseGoal.description = description;
+	courseGoal.completeUntit = date;
+	return courseGoal as CourseGoal;
 }
 
 /*
-Here the generic function works with a string
+Readonly is also a Utility type
+This array is readonly meaning we cannot push or pop anything to it
 */
 
-const textStorage = new DataStorage<string>();
-
-textStorage.addItem("Babies first textStorage");
-textStorage.addItem("A lying cake");
-textStorage.remoteItem("A lying cake");
-console.log(textStorage.getItems());
-
-/*
-Here the generic function works with a number
-*/
-
-const numberStorage = new DataStorage<number>();
-
-numberStorage.addItem(33);
-numberStorage.addItem(44);
-numberStorage.remoteItem(44);
-console.log(numberStorage.getItems());
-
-/*
-Union types also work as an example
-*/
-
-const unionStorage = new DataStorage<number | string>();
-
-/*
-This will not work since objects are not accepted as a datatype
-
-const objStorage = new DataStorage<object>();
-*/
+const names: Readonly<string[]> = ["David", "Ben"];
