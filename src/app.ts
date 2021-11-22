c031d93c43ed7c097df7b5700b89ac417ad6ea18
David Moll <david.leander.moll@gmail.com>
2021-11-22 13:09:54 +0100

    Added code/comments for 13.170

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("Address")! as HTMLInputElement;
+
+function searchAdressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAdress = addressInput.value;
+}
+
+form.addEventListener("submit", searchAdressHandler);
