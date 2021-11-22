34114555b8049956944f4c992d4e7ef6f2ee22de
David Moll <david.leander.moll@gmail.com>
2021-11-22 15:42:18 +0100

    Added code/comments for 13.172

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,45 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-const GOOGLE_API_KEY = "Wouldn't you like to know weather boy";
+const GOOGLE_API_KEY = "AIzaSyD7IJxB1ROF32N_TldLv3HmVXiY2BQu3Sk";
 
 function searchAdressHandler(event: Event) {
 	event.preventDefault();
 	const enteredAdress = addressInput.value;
-	console.log(enteredAdress);
+
+	/*
+	This HTTP-request returns:
+	results which is an array
+	where every object has a geometry key (also an object)
+	which has a location key (also an object)
+	which has a latitude-key of type number
+	and a longitude-key of type number
+	*/
+
+	type GoogleGeocodingResponse = {
+		results: { geometry: { location: { lat: number; lng: number } } }[];
+		status: "OK" | "ZERO_RESULTS";
+	};
+
+	axios
+		.get<GoogleGeocodingResponse>(
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+				enteredAdress
+			)}&key=${GOOGLE_API_KEY}`
+		)
+		.then((response) => {
+			if (response.data.status !== "OK") {
+				throw new Error("No location found");
+			}
+			const coordinates = response.data.results[0].geometry.location;
+			console.log(coordinates);
+		})
+		.catch((err) => {
+			alert(err.message);
+			console.log(err);
+		});
 }
 
 form.addEventListener("submit", searchAdressHandler);
