c1b12a6765a0acabd43710cae3270e6911e3f34c
David Moll <david.leander.moll@gmail.com>
2021-11-22 10:25:27 +0100

    Added code/comments for 11.156

--- dist/webpack.config.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=webpack.config.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require("path"); //No need to install, this is a core NodeJS-feature
 
 module.exports = {
-	entry: ".src/app.js",
+	entry: "./src/app.ts",
 	output: {
 		filename: "bundle.js",
-		path: path.resolve(__dirname, dist), //Constructs an absolute path to the dist-folder
+		path: path.resolve(__dirname, "dist"), //Constructs an absolute path to the dist-folder
 	},
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/, //looks for every file with the .ts-file extension
+                use: "ts-loader", //Any file should be handles by the ts-loader
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"] //Bundles all files with those extensions into one
+    }
 };
