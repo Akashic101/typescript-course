118ced773da4066e57571b145f5cd1d974b3edd9
David Moll <david.leander.moll@gmail.com>
2021-11-22 11:49:48 +0100

    Added code/comments for 12.163

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import _ from "lodash"; //lodash is build with vanilla JS for vanilla JS, not for TS. You need to install @types/lodash to make it work in ts
 
 console.log(_.shuffle([0, 1, 2, 3]));
+
+/*
+Normally TS does not know if a variable exists in another script. Declaring it
+makes sure that TS knows that it does exist and is now able to work with it
+*/
+
+declare var GLOBAL: any;
+
+console.log(GLOBAL);
