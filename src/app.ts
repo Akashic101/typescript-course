56332e27a92576ec328ab3ec23243d993fd48da5
David Moll <david.leander.moll@gmail.com>
2021-11-22 14:54:28 +0100

    Added code/comments for 13.171

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 const form = document.querySelector("form")!;
-const addressInput = document.getElementById("Address")! as HTMLInputElement;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "Wouldn't you like to know weather boy";
 
 function searchAdressHandler(event: Event) {
-    event.preventDefault();
-    const enteredAdress = addressInput.value;
+	event.preventDefault();
+	const enteredAdress = addressInput.value;
+	console.log(enteredAdress);
 }
 
 form.addEventListener("submit", searchAdressHandler);
