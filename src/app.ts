00036a046a68a93db37d77df6a06175f8f0515d3
David Moll <david.leander.moll@gmail.com>
2021-11-23 09:47:41 +0100

    Addded code/comments for 16.193

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
-import express from "express"; //When using the ES6 import you will get typescript-support
+import express, { Request, Response, NextFunction } from "express"; //When using the ES6 import you will get typescript-support
 // const express = require("express"); This is the commonJS import which is default in Node
 
+import todoRoutes from "./routes/todos";
+
 const app = express();
+
+app.use("/todos", todoRoutes);
+
+//Error-handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
